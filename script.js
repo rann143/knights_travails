64db67c2a01a27f1bce97449a96152c845d1f146
Daniel <roderm99@gmail.com>
2024-04-17 10:42:57 -0400

    initial commit of restart branch; create function to find available moves

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-class Knight {
+// Establish what are the legal coordinates (Our rules)
+    // Each of these coordinates are essentially 'nodes' in our graph
+    // How do I create this Graph???
+    
+// What we're doing is choosing a node to start from and doing a breadth-first traversal until we reach the node we desire
+    // Track which nodes we've already visited during traversal
+    
+// Then we isolate the shortest path of nodes from our starting to finishing node
 
-    constructor(position) {
-        this.position = position,
-        this.moves = getMoveOptions(position);
-        this.path = []
-    }
+function findAvailableMoves(c) {
+    // "C" is current coordinate (an array of two values -> x & y)
 
-}
+    const moves = [
 
-// Try to use recursion
-function knightMoves(startingPosition, endingPosition) {
-
-    let result = [];
-
-    if (startingPosition === endingPosition) return "Same Position";
-
-
-
-}
-
-// cuurentPosition will be an array with the X and Y coordinate representing the current position of the knight
-function getMoveOptions(currentPosition) {
-    const movesArray = [
-        [currentPosition[0] + 1, currentPosition[1] + 2],
-        [currentPosition[0] + 1, currentPosition[1] - 2],
-        [currentPosition[0] - 1, currentPosition[1] + 2],
-        [currentPosition[0] - 1, currentPosition[1] - 2],
-        [currentPosition[0] + 2, currentPosition[1] + 1],
-        [currentPosition[0] + 2, currentPosition[1] - 1],
-        [currentPosition[0] - 2, currentPosition[1] + 1],
-        [currentPosition[0] - 2, currentPosition[1] - 1],
+        [c[0] + 1, c[1] + 2],
+        [c[0] + 1, c[1] - 2],
+        [c[0] - 1, c[1] + 2],
+        [c[0] - 1, c[1] - 2],
+        [c[0] + 2, c[1] + 2],
+        [c[0] + 2, c[1] - 2],
+        [c[0] - 2, c[1] + 2],
+        [c[0] - 2, c[1] - 2],
+        
     ]
 
-    const legalMoves = movesArray.filter((move) => {
-        return move[0] < 8 && move[0] >= 0 && move[1] < 8 && move[1] >= 0;
+    const legalMoves = moves.filter(move => {
+        return move[0] >= 0 && move[0] < 8 && move[1] >= 0 && move[1] < 8
     })
 
     return legalMoves;
+}
+
+function traverseGraph(sc) {
+    // "sc" is starting coordinate
+    
 }
